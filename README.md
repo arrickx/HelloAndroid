ba828843f056a32c15abdd7806900bee72bf1d52
arrickx <24638167+arrickx@users.noreply.github.com>
2018-01-21 14:28:02 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# HelloAndroid
+* A single-screen app that displays information about Udacity,
+[![Image](image/udacityInfoPage.gif)](Image)
+
+## Getting Started
+
+* You can *[clone](TODO)* or *[download](TODO)* this project via [GitHub](https://github.com) to your local machine.
+* Download and Install *[Android Studio](https://developer.android.com/studio/index.html)*
+
+### How to use
+
+* Open existing Android Studio Project
+* Use Android Studio emulators to run this app
+* (Or use your Android phone to run this app via Android Studio)
+
+
+## License
+
+* This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Acknowledgments
+
+* **Udacity Image** was provided by [udacity](https://blog.udacity.com/2016/09/introducing-vr-developer-nanodegree-program.html).
+
+
---
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.android.helloandroid;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void insertContact(View view) {
+        Intent intent = new Intent(Intent.ACTION_INSERT);
+        intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+        intent.putExtra(ContactsContract.Intents.Insert.NAME, getString(R.string.udacity));
+        intent.putExtra(ContactsContract.Intents.Insert.PHONE, getString(R.string.telephone));
+        intent.putExtra(ContactsContract.Intents.Insert.EMAIL, getString(R.string.email));
+        intent.putExtra(ContactsContract.Intents.Insert.POSTAL, getString(R.string.address));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void sentEmail(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{getString(R.string.email)});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Hello, Udacity!");
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void dialPhoneNumber(View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + getString(R.string.telephone)));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+
+    public void direction(View view) {
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse("https://www.google.com/maps/search/?api=1&query=" + getString(R.string.address)));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void openWebPage(View view) {
+        Uri webpage = Uri.parse("https://www.udacity.com");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+
+}
